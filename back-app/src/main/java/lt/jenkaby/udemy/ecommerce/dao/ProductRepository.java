5d0c8441e83a5071c62059233691ec7ed27fb089
yauheh.k <evgen.kuzmich@gmail.com>
2024-09-06 08:57:17 +0300

    feat-be: add cross origin to exposed endpoints

--- back-app/src/main/java/lt/jenkaby/udemy/ecommerce/dao/ProductRepository.java
+++ back-app/src/main/java/lt/jenkaby/udemy/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package lt.jenkaby.udemy.ecommerce.dao;
 
 import lt.jenkaby.udemy.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(maxAge = 3600)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
